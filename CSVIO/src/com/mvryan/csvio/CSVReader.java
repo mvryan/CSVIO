a2ffd633577c2ab4055c279ee1000b7b037ee33c
Michael Ryan <mik3.ry4n@gmail.com>
2011-10-29 13:13:43 -0400

    More foundational work

--- CSVIO/src/com/mvryan/csvio/CSVReader.java
+++ CSVIO/src/com/mvryan/csvio/CSVReader.java
@@ -3,28 +3,53 @@ package com.mvryan.csvio;
 import java.io.*;
 import java.util.*;
 
+/**
+ * 
+ * @author Michael Ryan
+ *
+ */
 public class CSVReader implements Closeable {
 
 	private BufferedReader reader;
 	private InputStream inputStream;
 	
+	/**
+	 * 
+	 * @param filePath
+	 * @throws FileNotFoundException
+	 */
 	public CSVReader(String filePath) throws FileNotFoundException{
 		this(new FileInputStream(filePath));
 	}
 	
+	/**
+	 * 
+	 * @param inputStream
+	 */
 	public CSVReader(InputStream inputStream){
 		this.inputStream = inputStream;
 		reader = new BufferedReader(new InputStreamReader(inputStream));
 	}
 	
+	/**
+	 * 
+	 */
 	public void reset(){
 		reader = new BufferedReader(new InputStreamReader(inputStream));
 	}
 
+	/**
+	 * 
+	 */
 	public void close() throws IOException {
 		reader.close();
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
 	public List<String> nextRow() throws IOException{
 		List<String> result = new ArrayList<String>();
 		StringBuilder stringBuilder = new StringBuilder();
@@ -32,33 +57,32 @@ public class CSVReader implements Closeable {
 		if (line == null) {
 			return null;
 		}
-		char[] chars = line.toCharArray();
 		int state = 1;
-		for (int i = 0; i < chars.length; i++) {
+		for (char c : line.toCharArray()) {
 			switch (state) {
 			case 1:
-				if (chars[i] == ',') {
+				if (c == ',') {
 					result.add(stringBuilder.toString());
 					stringBuilder = new StringBuilder();
-				} else if (chars[i] == '"') {
+				} else if (c == '"') {
 					state = 2;
 				} else {
-					stringBuilder.append(chars[i]);
+					stringBuilder.append(c);
 				}
 				break;
 			case 2:
-				if (chars[i] == '"') {
+				if (c == '"') {
 					state = 3;
 				} else {
-					stringBuilder.append(chars[i]);
+					stringBuilder.append(c);
 				}
 				break;
 			case 3:
-				if (chars[i] == ',') {
+				if (c == ',') {
 					result.add(stringBuilder.toString());
 					stringBuilder = new StringBuilder();
 					state = 1;
-				} else if (chars[i] == '"') {
+				} else if (c == '"') {
 					stringBuilder.append('"');
 					state = 2;
 				} else {
@@ -71,6 +95,11 @@ public class CSVReader implements Closeable {
 		return result;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
 	public List<List<String>> getTable() throws IOException{
 		List<List<String>> table = new ArrayList<List<String>>();
 		List<String> row;
--- CSVIO/src/com/mvryan/csvio/CSVWriter.java
+++ CSVIO/src/com/mvryan/csvio/CSVWriter.java
@@ -1,6 +1,96 @@
 package com.mvryan.csvio;
 
-public class CSVWriter {
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ * @author Michael Ryan
+ *
+ */
+public class CSVWriter implements Closeable, Flushable {
+
+	private Writer writer;
+	
+	/**
+	 * 
+	 * @param filePath
+	 * @throws FileNotFoundException
+	 */
+	public CSVWriter(String filePath) throws FileNotFoundException{
+		this(new FileOutputStream(filePath));
+	}
+	
+	/**
+	 * 
+	 * @param outputStream
+	 */
+	public CSVWriter(OutputStream outputStream){
+		writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+	}
+	
+	/**
+	 * 
+	 */
+	public void flush() throws IOException{
+		writer.flush();
+	}
+	
+	/**
+	 * 
+	 */
+	public void close() throws IOException{
+		writer.close();
+	}
+	
+	/**
+	 * 
+	 * @param row
+	 * @throws IOException
+	 */
+	public void writeRow(List<String> row) throws IOException{
+		StringBuilder rowBuilder = new StringBuilder();
+		boolean first = true;
+		for (String entry : row) {
+			StringBuilder elementBuilder = new StringBuilder();
+			boolean quoteFlag = false;
+			for (char c : entry.toCharArray()) {
+				if (c == '"') {
+					quoteFlag = true;
+					elementBuilder.append("\"\"");
+				} else if (c == ',') {
+					quoteFlag = true;
+					elementBuilder.append(',');
+				} else {
+					elementBuilder.append(c);
+				}
+			}
+			if(!first){
+				rowBuilder.append(',');
+			}
+			if(quoteFlag){
+				rowBuilder.append('"');
+			}
+			rowBuilder.append(elementBuilder);
+			if(quoteFlag){
+				rowBuilder.append('"');
+			}
+			first = false;
+		}
+		rowBuilder.append("\n");
+		writer.write(rowBuilder.toString());
+	}
+	
+	/**
+	 * 
+	 * @param table
+	 * @throws IOException
+	 */
+	public void writeTable(List<List<String>>table) throws IOException{
+		for(List<String> row : table){
+			writeRow(row);
+		}
+	}
 
 }
 
